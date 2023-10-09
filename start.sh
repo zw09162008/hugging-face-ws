1e0fdd83aa48224c237038d8c3634275ebc8e340
zw09162008 <zw09162008@gmail.com>
2023-10-09 23:10:09 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
-export NEZHA_SERVER="xxx.xxxx.com:5555"
-export NEZHA_KEY="d0hJ9XrXSb1abcdefg"
+export NEZHA_SERVER=""
+export NEZHA_KEY=""
 
 chmod +x server start.sh
 nohup ./server -s ${NEZHA_SERVER} -p ${NEZHA_KEY} > /dev/null 2>&1 &   #！若需要tls，在此句 > 前面加上--tls即可
